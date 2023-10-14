933a8d375919e1c242875dcabde0eff9adb0e5bb
anhgit-91 <tadang.developer@gmail.com>
2023-10-14 13:04:13 +1100

    accordion app

---
+++ accordion/src/Question.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+
+const Question = ({title, info}) => {
+   const [showInfo, setShowInfo] = useState(false)
+    return ( 
+        <article className="question">
+            <header>
+                <h4>{title}</h4>
+                <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+                    {!showInfo ? <AiOutlinePlus/> : <AiOutlineMinus /> }
+                </button>
+            </header>
+            <div className={`info ${showInfo ? 'show' : ''}`}>
+            {
+                showInfo && <p>{info}</p>
+            }
+            </div>
+            
+            
+        </article>
+     );
+}
+
+export default Question;
