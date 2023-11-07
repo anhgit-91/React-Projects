34d2da3caaa0b4dc72d152757d61a22f26d98e29
anhgit-91 <tadang.developer@gmail.com>
2023-11-07 15:51:11 +1100

    Context for products

--- weather-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-orange-400 via-violet-400 to-green-400 h-[100vh] w-full">
-      Hello world
-    </main>
-  )
-}
-
-export default App
